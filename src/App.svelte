de1862a2aebd46069dbaa3e7f5ca3e25c30ced3f
Gilles CRUCHON <gilles.cruchon@axa.com>
2021-12-18 10:54:21 +0100

    feat: personalized title and index

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,9 @@
 <script>
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Gilles CRUCHON's certification path</h1>
+	<p>To be completed.</p>
 </main>
 
 <style>
@@ -13,13 +12,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
