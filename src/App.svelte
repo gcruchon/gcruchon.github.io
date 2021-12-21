c5e87df28715ab7a1af2a5796d3d918f8b12b947
Gilles CRUCHON <gilles.cruchon@axa.com>
2021-12-21 11:08:14 +0100

    feat: add certs and trainings

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,119 @@
 <script>
+  import { Alert, Col, Container, Row } from "sveltestrap";
+  import Badge from "./Badge.svelte";
 </script>
 
-<main>
-	<h1>Gilles CRUCHON's certification path</h1>
-	<p>To be completed.</p>
-</main>
+<Container>
+  <h1 class="display-4">Gilles CRUCHON</h1>
+  <h2 class="text-secondary mb-5">Cloud training / cert path</h2>
+  <Row>
+    <Col class="text-center">
+      <Alert color="primary"><h3>Azure</h3></Alert>
+    </Col>
+    <Col class="text-center">
+      <Alert color="success"><h3>Google Cloud Platform</h3></Alert>
+    </Col>
+  </Row>
+  <Row class="cert">
+    <Col>
+      <Container>
+        <Row>
+          <Col>
+            <Badge
+              name="azure-fundamentals"
+              certName="AZ-900"
+              url="https://www.credly.com/badges/498e248a-0ca6-4bdb-ac9c-4071da44c86a/public_url"
+            />
+          </Col>
+          <Col>
+            <Badge name="azure-data-engineer-associate" certName="DP-203" />
+          </Col>
+          <Col>
+            <Badge name="azure-ai-engineer-associate" certName="AI-102" />
+          </Col>
+        </Row>
+      </Container>
+    </Col>
+    <Col>
+      <Container>
+        <Row>
+          <Col>
+            <Badge
+              name="gcp-cloud-engineer-associate"
+              certName="ASL Advanced Machine Learning Ops with TFX"
+              url="#"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Col>
+  </Row>
+  <Row>
+    <Col class="text-center">
+      <Alert color="warning"><h3>Amazon Web Services</h3></Alert>
+    </Col>
+    <Col class="text-center"
+      ><Alert color="danger"><h3>Red Hat OpenShift</h3></Alert></Col
+    >
+  </Row>
+  <Row class="cert">
+    <Col>
+      <Container>
+        <Row>
+          <Col>
+            <Badge
+              name="aws-certified-cloud-practitioner"
+              certName="AWS Certified Cloud Practitioner"
+            />
+          </Col>
+          <Col>
+            <Badge
+              name="aws-certified-machine-learning-specialty"
+              certName="AWS Certified Machine Learning – Specialty"
+            />
+          </Col>
+          <Col>
+            <Badge
+              name="aws-certified-data-analytics-specialty"
+              certName="AWS Certified Data Analytics - Specialty"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Col>
+    <Col>
+      <Container>
+        <Row>
+          <Col>
+            <Badge
+              name="redhat-openshift"
+              certName="DO-180 - Containers & Kubernetes"
+              url="#"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Col>
+  </Row>
+</Container>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  h3 {
+    font-size: 1.5em;
+    margin: 0;
+    padding: 0;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  :global(.cert) {
+    margin: 0.2em 0 1em 0;
+  }
+
+  :global(.cert .col) {
+    margin: 0.7em;
+    text-align: center;
+  }
+
+  :global(.cert .col img) {
+    max-width: 150px;
+  }
 </style>
---
+++ src/Badge.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { Image } from "sveltestrap";
+  export let certName;
+  export let name;
+  export let url = false;
+
+  let src = "/img/" + name;
+  let certIdClassName = "text-secondary";
+  let certIcom = "";
+  if (url) {
+    src += "-passed";
+    certIdClassName = "fw-bold";
+    certIcom = "&nbsp;✅"
+  }
+  src += ".png";
+</script>
+
+{#if url && url != "#"}
+  <a href={url} target="_blank"><Image {src} /></a>
+{:else}
+  <Image {src} />
+{/if}
+<div class={certIdClassName}>{certName}{@html certIcom}</div>
